8a60e1f0b741406ab1bb9777783d579c6917dbd5
Guilherme Fernandes Secco <155193683+GuilhermeSecco@users.noreply.github.com>
2025-07-14 17:48:59 -0300

    Dando commit no .sql também

---
+++ Treino.sql
@@ -0,0 +1,35 @@
+CREATE TABLE pessoas (
+id int NOT NULL auto_increment,
+nome varchar(30) NOT NULL,
+nascimento date,
+sexo enum('M', 'F'),
+peso decimal(5,2),
+altura decimal(3,2),
+nacionalidade varchar(20) DEFAULT 'Brasil',
+primary key (id)
+)DEFAULT CHARSET = utf8;
+
+INSERT INTO pessoas
+(nome, nascimento, sexo, peso, altura, nacionalidade)
+VALUES
+('Godofredo', '1984-01-02', 'M', '78.5', '1.83', 'Brasil');
+
+INSERT INTO pessoas
+(nome, nascimento, sexo, peso, altura, nacionalidade)
+VALUES
+('Maria', '1999-12-30', 'F', '55.2', '1.65', 'Portugal');
+
+INSERT INTO pessoas
+(nome, nascimento, sexo, peso, altura, nacionalidade)
+VALUES
+('Creusa', '1920-7-12', 'F', '48.9', '1.55', 'Brasil');
+
+INSERT INTO pessoas VALUES
+(DEFAULT, 'Valter', '1955-1-5', 'M', '88.2', '1.80', 'Bolivia');
+
+INSERT INTO pessoas VALUES
+(DEFAULT, 'Bob', '1978-4-7', 'M', '104.2', '1.70', 'Estados Unidos'),
+(DEFAULT, 'Ana', '2005-3-25', 'F', '57.2', '1.55', 'Canada'),
+(DEFAULT, 'Richard', '1991-11-28', 'M', '78.5', '1.87', 'Inglaterra');
+
+select * from pessoas;
