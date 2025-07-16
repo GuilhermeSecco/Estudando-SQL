25db138b50eafbe26158712aaca4a9682af7d9d1
Guilherme Fernandes Secco <155193683+GuilhermeSecco@users.noreply.github.com>
2025-07-16 17:11:11 -0300

    Praticando Update/Delete/Truncate

--- Treino.sql
+++ Treino.sql
@@ -63,4 +63,41 @@ add column idcurso int first;
 alter table cursos
 add primary key(idcurso);
 
-describe cursos;
+alter table cursos
+modify column idcurso int auto_increment;
+
+insert into cursos values
+(default,'HTML4', 'Curso de HTML5', '40', '37', '2014'),
+(default,'Algoritmos', 'Lógica de Programação', '20', '15', '2014'),
+(default,'Photoshop', 'Dicas de Photoshop CC', '10', '8', '2014'),
+(default,'PGP', 'Curso de PHP para iniciantes', '40', '20', '2010'),
+(default,'Jarva', 'Introdução à Linguagem Java', '10', '29', '2000'),
+(default,'MySQL', 'Bancos de Dados MySQL', '30', '15', '2016'),
+(default,'Word', 'Curso completo de Word', '40', '30', '2018'),
+(default,'Sapateado', 'Danças Rítmicas', '40', '30', '2018'),
+(default,'Cozinha Árabe', 'Aprenda a fazer Kibe', '40', '30', '2018'),
+(default,'YouTuber', 'Gerar polêmica e ganhar inscritos', '5', '2', '2018');
+
+update cursos
+set nome = 'HTML5'
+where idcurso = '1';
+
+update cursos
+set nome = 'PHP', ano = '2015'
+where idcurso = '4';
+
+update cursos
+set nome = 'Java', horas = '40', ano = '2015'
+where idcurso = '5'
+limit 1;
+
+delete from cursos
+where idcurso = '8';
+
+delete from cursos
+where idcurso = '9';
+
+delete from cursos
+where idcurso = '10';
+
+truncate cursos;
